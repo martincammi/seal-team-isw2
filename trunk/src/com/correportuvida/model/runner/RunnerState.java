a202dcfb54ffab7e71fda6659dcccc08cac04351
martincammi <martincammi@4c227ddb-cf14-f304-b474-24587ef48d6c>
2013-10-04 12:24:31 +0000

    RunnerState es abstracto ahora.

--- trunk/src/com/correportuvida/model/runner/RunnerState.java
+++ trunk/src/com/correportuvida/model/runner/RunnerState.java
@@ -1,5 +1,21 @@
 package com.correportuvida.model.runner;
 
-public class RunnerState {
-
+public abstract class RunnerState {
+	
+	private String _name;
+	private int value;
+	
+	public String get_name() {
+		return _name;
+	}
+	public void set_name(String _name) {
+		this._name = _name;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
 }
